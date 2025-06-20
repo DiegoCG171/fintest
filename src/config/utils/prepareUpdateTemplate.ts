acfd485f5a47b2bcc7c8d8fcd13971e02b69146d
Karim Trujillo <karim.trujillo@memphis.mx>
2025-06-19 23:07:47 -0600

    fix(*):delet only child's DE-63 when are !isRequired

--- src/config/utils/prepareUpdateTemplate.ts
+++ src/config/utils/prepareUpdateTemplate.ts
@@ -4,16 +4,17 @@ import {
     TableRowDataFormBuilder,
 } from "../interfaces";
 
-// Recursivo sin filtrar hijos
 function mapNestedRows(rows: TableRowDataFormBuilder[]): FieldUpdateTemplate[] {
-    return rows.map((row) => ({
-        idBitmap: row.idBitmap ?? '',
-        isRequired: Boolean(row.isRequired),
-        function: row.function ?? '',
-        value: row.value ?? '',
-        fields: Array.isArray(row.breakingRules)
-        ? mapNestedRows(row.breakingRules as TableRowDataFormBuilder[])
-        : undefined,
+    return rows
+        .filter((row) => row.isRequired)
+        .map((row) => ({
+            idBitmap: row.idBitmap ?? '',
+            isRequired: true,
+            function: row.function ?? '',
+            value: row.value ?? '',
+            fields: Array.isArray(row.breakingRules)
+            ? mapNestedRows(row.breakingRules as TableRowDataFormBuilder[])
+            : [],
     }));
 }
 
@@ -23,16 +24,20 @@ export function prepareUpdatePayload(
     ): PatchGenerationTemplate {
     const formattedRows: FieldUpdateTemplate[] = rows
         .filter((row) => row.isActive)
-        .filter((row) => !(row.idBitmap === "DE-63" && !row.isRequired))
-        .map((row) => ({
-        idBitmap: row.idBitmap ?? '',
-        isRequired: Boolean(row.isRequired),
-        function: row.function ?? '',
-        value: row.value ?? '',
-        fields: Array.isArray(row.breakingRules)
-            ? mapNestedRows(row.breakingRules as TableRowDataFormBuilder[])
-            : undefined,
-        }));
+        .map((row) => {
+        let fields: FieldUpdateTemplate[] = [];
+        if (row.idBitmap === "DE-63" && Array.isArray(row.breakingRules)) {
+            fields = mapNestedRows(row.breakingRules as TableRowDataFormBuilder[]);
+        }
+
+        return {
+            idBitmap: row.idBitmap ?? '',
+            isRequired: Boolean(row.isRequired),
+            function: row.function ?? '',
+            value: row.value ?? '',
+            fields,
+        };
+        });
 
     return {
         [typeForm]: formattedRows,
