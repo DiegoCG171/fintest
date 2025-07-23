4e5ee6491d89612372416e643ba18108edb740c8
Karim Trujillo <karim.trujillo@memphis.mx>
2025-07-23 15:25:17 -0600

    fix(*):tab navigation fixed

--- src/pages/DecisionComponent.tsx
+++ src/pages/DecisionComponent.tsx
@@ -106,7 +106,7 @@ function DecisionComponent() {
         <Button
           variant="contained"
           component={Link}
-          to="/pos/acquirer"
+          to="/pos/acquirer/detalles"
           sx={{ minWidth: 160 }}
           onClick={() => {
             dispatch(getCategoriesByMethodThunk("pos/acquirer"))
@@ -122,7 +122,7 @@ function DecisionComponent() {
         <Button
           variant="contained"
           component={Link}
-          to="/atm/acquirer"
+          to="/atm/acquirer/detalles"
           sx={{ minWidth: 160 }}
           onClick={() => {
             dispatch(getCategoriesByMethodThunk("atm/acquirer"))
