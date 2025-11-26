6470765833264717eee92fa7e7a9491c2c21bfdc
DiegoCG171 <diego.cerongutierrez@gmail.com>
2025-11-26 13:45:34 -0600

    fix(*): add idRoles in payload createUser

--- src/store/slices/users/user.thunk.ts
+++ src/store/slices/users/user.thunk.ts
@@ -9,6 +9,7 @@ export const createUserThunk = createAsyncThunk<
 >(
     'users/create',
     async (user: createUserInterface, { rejectWithValue }) => {
+      console.log(user)
         try {
           return await createUser(user)
         } catch (error) {
